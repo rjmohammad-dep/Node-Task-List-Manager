38e9ffd3d4ca1519094b857674ce66b8339392e7
RJ Mohammad <rjmohammad@outlook.com>
2017-05-29 14:57:08 -0400

    intialize server and client files

---
+++ client.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+const readline = require('readline');
+const server = require('./server');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const client = new EventEmitter ();
---
+++ server.js
@@ -0,0 +1,6 @@
+const EventEmitter = require('events');
+class Server extends EventEmitter {
+
+}
+
+module.export = new Server();
